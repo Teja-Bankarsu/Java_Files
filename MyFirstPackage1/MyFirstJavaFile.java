f56e8958184330282df1b19efa1b1fbb97b7c150
Teja_Bankarsu <bankarsu.sai@gmail.com>
2025-04-22 22:30:13 +0530

    Sending my Java Files to Github remote

---
+++ MyFirstPackage1/MyFirstJavaFile.java
@@ -0,0 +1,8 @@
+package MyFirstPackage1;
+
+public class MyFirstJavaFile
+{
+    public static void main(String[] args) {
+        System.out.println("Human: Hi Computer, Welcome to Beautiful World(Earth)");
+    }
+}
